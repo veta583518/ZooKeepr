afd13f7e8a9cd4684476f18d78e82ba7fabc3b9c
CoCo Chanel <veta583518@gmail.com>
2021-04-16 20:45:02 -0500

    added ability for user creation of data, validation for the data, and adding data to catalog

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 const { animals } = require("./data/animals.json");
 
+// function to query animalsArray by personality traits, diet, name, and/or species
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   // Note that we save the animalsArray as filteredResults here:
@@ -47,13 +54,51 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+// function to return specific animal object
 function findById(id, animalsArray) {
   const result = animalsArray.filter((animal) => animal.id === id)[0];
   return result;
 }
 
+// function to create new animal, push animal to animalsArray, create data and add it to the catalog
+function createNewAnimal(body, animalsArray) {
+  // our function's main code will go here!
+  const animal = body;
+
+  animalsArray.push(animal);
+
+  // write to animal.json file in the data subdirectory
+  fs.writeFileSync(
+    // use path.join() to join the value of the directory of the file we will execute the code in with the path to the json file
+    path.join(__dirname, "./data/animals.json"),
+
+    // save the js array as JSON; non edited and with whitespace for easy readability
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  // return finished code to post route for response
+  return animal;
+}
+
+// add validation to make sure new animal data exists and also is the right type of data
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 app.get("/api/animals", (req, res) => {
   let results = animals;
+
   if (req.query) {
     results = filterByQuery(req.query, results);
   }
@@ -62,10 +107,26 @@ app.get("/api/animals", (req, res) => {
 
 app.get("/api/animals/:id", (req, res) => {
   const result = findById(req.params.id, animals);
+
   if (result) {
     res.json(result);
   } else {
     res.send(404);
+  }
+});
+
+app.post("/api/animals", (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+
+    res.json(animal);
   }
 });
 
